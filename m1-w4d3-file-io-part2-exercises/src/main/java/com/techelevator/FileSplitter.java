68c44894cc516d8b3e3c463b025faf7be336f74b
Sam Niehaus <sen1019@gmail.com>
2017-09-20 18:23:33 -0400

    w4d3 Activities Commit

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
@@ -1,8 +1,70 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+//   /Users/samniehaus/workspace/exercises/m1-w4d3-file-io-part2-exercises/FizzBuzz.txt
+
 public class FileSplitter {
 
 	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+			
+		System.out.println("Please enter the number of lines you want in each .txt file. ");
+		int fileSize = 0;
+		try{
+			fileSize = Integer.parseInt(userInput.nextLine());
+			if(fileSize < 1){
+				System.out.println("Too small.");
+				System.exit(1);
+			}
+		}catch(NumberFormatException e){
+			System.out.println("Your input didn't have an integer.");
+			System.exit(2);
+		}
+		
+		System.out.println("Please provide the file path to the source file. ");
+		String path = userInput.nextLine();
+		File inputFile = new File(path);
+		if(!inputFile.exists()){
+			System.out.println("Could not locate source file.");
+			System.exit(3);
+		}
+		
+		splitFile(path, fileSize);
+			
+	}
+	
+	public static void splitFile(String path, int maxLines){
+		int delineator = path.lastIndexOf("/");
+		String fileName = path.substring(delineator + 1, path.length()-4);
+		
+		try(Scanner fileReader = new Scanner(new File(path))){
+			int i = 0;
+			String line = "";
+			while(fileReader.hasNextLine()){
+				try(PrintWriter fileWriter = new PrintWriter("./" + fileName + "-" + i + ".txt")){
+					int j = 0;
+					while(j < maxLines && fileReader.hasNextLine()){
+						line = fileReader.nextLine();
+						if(j == maxLines -1){
+							fileWriter.print(line);
+						}else{
+							fileWriter.println(line);
+						}
+						j++;
+					}
+				}
+				i++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.print("The file has disappeared.");
+			System.exit(4);
+		}
+		
 		
 
 	}
--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,31 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
-		
-
+		try(PrintWriter writer = new PrintWriter(new File("./FizzBuzz.txt"))){
+			for(int i = 1; i <= 300; i++){
+				if (i % 3 == 0){
+					if(i % 5 == 0){
+						writer.println("FizzBuzz");
+					}else{
+						writer.println("Fizz");
+					}
+				}else if(i % 5 == 0){
+					writer.println("Buzz");
+				}else{
+					writer.println(i);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.getMessage();
+			System.exit(1);
+		}
 	}
-
 }
